3413790f7c24e2cbcc3bf7f0f514f318482cf93d
Sugam Sahu <90542591+iSugam@users.noreply.github.com>
2026-05-14 20:01:53 +0530

    Update with react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import CarouselFour from "./CarouselFour";
-import GSAPCarousel from "./GSAPCarousel";
+import { Route, Routes } from "react-router";
+import SuperFlowClone from "./SuperFlowClone";
 import SwiperJs from "./SwiperJs";
 
 function App() {
   return (
     <>
-      <SwiperJs />
-      {/* <GSAPCarousel /> */}
-      {/* <CarouselFour /> */}
+      <Routes>
+        <Route path="/" element={<SwiperJs />} />
+
+        <Route path="two" element={<SuperFlowClone />} />
+      </Routes>
     </>
   );
 }
