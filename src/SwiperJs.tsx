27a860a78656b7e99003bc3b9dfad8adabdfe2ac
Sugam Sahu <90542591+iSugam@users.noreply.github.com>
2026-05-14 15:50:28 +0530

    Update animations

--- src/SwiperJs.tsx
+++ src/SwiperJs.tsx
@@ -60,10 +60,10 @@ export default function App() {
         creativeEffect={{
           prev: {
             shadow: true,
-            translate: [0, 0, 0],
+            translate: ["-50%", 0, 0],
           },
           next: {
-            translate: ["100%", 0, 1],
+            translate: ["100%", 0, 0],
           },
         }}
         modules={[EffectCreative, Autoplay]}
